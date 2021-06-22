c888ad4eabc44e2dee163303a89941c0beaf0dab
carpmatias <cardenasmatias.1990@gmail.com>
2021-06-22 20:12:38 +0200

    Updating page object

--- cypress/page-objects/specific-feed-page.js
+++ cypress/page-objects/specific-feed-page.js
@@ -4,6 +4,14 @@ export class SpecificFeedPage {
 
     endpoint = "feeds/"
 
-    navigate(feedId) { cy.visit(`/${this.endpoint}/${feedId}`)}
+    navigate(feedId) { cy.visit(`/${this.endpoint}${feedId}`)}
+
+    checkForUpdatesButton() { return cy.get('.text-right > .btn') }
+
+    bookmarkButton() { return cy.get('[href="/feeds/1/toggle-bookmark/"] > .glyphicon') }
+
+    addedByField() { return cy.get('.dl-horizontal > :nth-child(2)') }
+
+    feedUrlField() { return cy.get('.dl-horizontal > :nth-child(4)') }
     
 }
