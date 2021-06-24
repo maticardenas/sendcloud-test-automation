52b5635ad702556b0b5414671500d5c85aeb4144
carpmatias <cardenasmatias.1990@gmail.com>
2021-06-24 14:44:37 +0200

    Adding page object for feeds entry page

---
+++ cypress/page-objects/feeds-entry-page.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+export class FeedsEntryPage {
+
+    endpoint = "/entry/"
+
+    navigate(feedEntryId) { cy.visit(`/feeds/${feedEntryId}${this.endpoint}`)}
+
+    commentField() { return cy.get('.CodeMirror') }
+
+    commentAdded(commentNumber) { return cy.get(`#comment-${commentNumber}`) } 
+
+    submitButton() { return cy.get('#submit-id-submit') }
+    
+    alertMessage() { return cy.get('.alert') }
+
+    titleLink(feedNumber){
+        return cy.get(`tbody > :nth-child(${feedNumber}) > :nth-child(1) > a`)
+    }
+
+}
